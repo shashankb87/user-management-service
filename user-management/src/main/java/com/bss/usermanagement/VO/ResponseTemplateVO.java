a22cd9c2b5150be14ab5de613aaf99595be18e6f
Shashank Bettadapura <106806734+shashankb87@users.noreply.github.com>
2022-09-15 11:45:24 +0530

    Create ResponseTemplateVO.java

---
+++ user-management/src/main/java/com/bss/usermanagement/VO/ResponseTemplateVO.java
@@ -0,0 +1,15 @@
+package com.bss.usermanagement.VO;
+
+import com.bss.usermanagement.entity.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseTemplateVO {
+    private User user;
+    private Department department;
+}
