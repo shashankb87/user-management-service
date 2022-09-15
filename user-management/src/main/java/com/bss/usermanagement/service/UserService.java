303189cead7ad8a8e8d331dd84843a34db190c80
Shashank Bettadapura <106806734+shashankb87@users.noreply.github.com>
2022-09-15 11:45:44 +0530

    Create UserService.java

---
+++ user-management/src/main/java/com/bss/usermanagement/service/UserService.java
@@ -0,0 +1,43 @@
+package com.bss.usermanagement.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.bss.usermanagement.VO.Department;
+import com.bss.usermanagement.VO.ResponseTemplateVO;
+import com.bss.usermanagement.entity.User;
+import com.bss.usermanagement.repository.UserRepo;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class UserService{
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public User saveUser(User user) {
+        log.info("Department Service - Saving - " + user);
+        return userRepo.save(user);
+    }
+
+    public User findUser(Long userId) {
+        log.info("Finding user Id: " + userId);
+        return userRepo.findById(userId).get();
+    }
+
+    public ResponseTemplateVO getUserAndDepartment(Long userId) {
+        log.info("Find user and department info for userid: " + userId);
+        ResponseTemplateVO vo = new ResponseTemplateVO();
+        User user = userRepo.findById(userId).get();
+        Department dept = restTemplate.getForObject("http://localhost:9001/departments/" + user.getUserId(), Department.class);
+        vo.setUser(user);
+        vo.setDepartment(dept);
+        return vo;
+    }
+}
