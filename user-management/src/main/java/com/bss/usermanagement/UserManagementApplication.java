e89b80c85e08b93923cefd19a2806311aeb2aadd
Shashank Bettadapura <106806734+shashankb87@users.noreply.github.com>
2022-09-15 11:44:38 +0530

    Create UserManagementApplication.java

---
+++ user-management/src/main/java/com/bss/usermanagement/UserManagementApplication.java
@@ -0,0 +1,20 @@
+package com.bss.usermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class UserManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementApplication.class, args);
+	}
+
+	@Bean
+	private RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+
+}
