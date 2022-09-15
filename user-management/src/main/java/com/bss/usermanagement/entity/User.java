1fbbce3e4d7c7eab5eb8bebda0c2d03e907fd6a5
Shashank Bettadapura <106806734+shashankb87@users.noreply.github.com>
2022-09-15 11:46:17 +0530

    Create User.java

---
+++ user-management/src/main/java/com/bss/usermanagement/entity/User.java
@@ -0,0 +1,24 @@
+package com.bss.usermanagement.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userId;
+    private String userName;
+    private String userAddress;
+    private String departmentCode;
+}
